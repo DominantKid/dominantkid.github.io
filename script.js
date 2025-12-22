edb05d2f22121c1f6e026f385c44facbd43ce6c1
Felipickle <felipe.antonio_jaramillo@outlook.com>
2025-12-22 09:41:46 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
-// Optional future JS: search, filter, or other interactivity
-console.log("Pokémon Hub loaded!");
+<script>
+let visits = localStorage.getItem("visits") || 0;
+visits++;
+localStorage.setItem("visits", visits);
+document.write("<p>You have visited this page " + visits + " times.</p>");
+</script>
