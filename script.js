e4947692fff5b58775836488ec4f5431bceff645
Felipickle <felipe.antonio_jaramillo@outlook.com>
2025-12-24 22:59:03 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,1 @@
-<script>
-let visits = localStorage.getItem("visits") || 0;
-visits++;
-localStorage.setItem("visits", visits);
-document.write("<p>You have visited this page " + visits + " times.</p>");
-</script>
+
